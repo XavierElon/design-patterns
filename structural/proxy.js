5f5952f6d60ff8d6c15b85bd5fb27a6cccbb5b48
XavierElon <xavierelon93@gmail.com>
2024-07-09 10:45:41 -0400

    proxy pattern

--- structural/proxy.js
+++ structural/proxy.js
@@ -1,81 +1,40 @@
-// Adaptee
-class FrenchSpeaker {
-  speakFrench(message) {
-    console.log('Speaking in Frnech: ' + message)
+// Base class acting as an interface
+class DatabaseQuery {
+  // Method to be overridden by subclasses
+  executeQuery(query) {
+    throw new Error('executeQuery method must be overridden')
   }
 }
 
-// Target Interface
-class EnglishSpeaker {
-  speakEnglish(message) {
-    console.log('Speaking in English: ' + message)
+// Class that performs the actual query execution
+class RealDatabaseQuery extends DatabaseQuery {
+  executeQuery(query) {
+    console.log(`Executing database query: ${query}`)
+    return `Result of ${query}`
   }
 }
 
-// Adapter
-class Translator extends EnglishSpeaker {
-  constructor(frenchSpeaker) {
+// Class that adds caching functionality
+class CachedDatabaseQuery extends DatabaseQuery {
+  constructor() {
     super()
-    this.frenchSpeaker = frenchSpeaker
+    this.realQuery = new RealDatabaseQuery()
+    this.cache = {}
   }
 
-  speakFrench(message) {
-    let frenchMessage = this.translateToFrench(message)
-    this.frenchSpeaker.speakFrench(frenchMessage)
-  }
-
-  speakEnglish(message) {
-    let englishMessage = this.translateToEnglish(message)
-    this.englishSpeaker.speakEnglish(englishMessage)
-  }
-
-  translateToFrench(message) {
-    if (message === 'Hello') {
-      return 'Bonjour'
-    } else if (message === 'Goodbye') {
-      return 'Au revoir'
+  executeQuery(query) {
+    if (this.cache[query]) {
+      console.log(`Returning cached result for query: ${query}`)
+      return this.cache[query]
+    } else {
+      const result = this.realQuery.executeQuery(query)
+      this.cache[query] = result
+      return result
     }
-    return message
-  }
-
-  translateToEnglish(message) {
-    if (message === 'Bonjour') {
-      return 'Hello'
-    } else if (message === 'Au revoir') {
-      return 'Goodbye'
-    }
-    return message
   }
 }
 
-// Client
-class EnglishClient {
-  constructor(speaker) {
-    this.speaker = speaker
-  }
-
-  express(message) {
-    this.speaker.speakEnglish(message)
-  }
-}
-
-class FrenchClient {
-  constructor(speaker) {
-    this.speaker = speaker
-  }
-
-  express(message) {
-    this.speaker.speakFrench(message)
-  }
-}
-
-// Demo
-const frenchSpeaker = new FrenchSpeaker()
-const englishSpeaker = new EnglishSpeaker()
-const translator = new Translator(frenchSpeaker)
-const client = new EnglishClient(translator)
-translator.speakEnglish('Hello')
-translator.speakFrench('Bonjour')
-
-client.express('Hello')
-client.express('Goodbye')
+// Client code
+const query = new CachedDatabaseQuery()
+console.log(query.executeQuery('SELECT * FROM users')) // Executes and caches
+console.log(query.executeQuery('SELECT * FROM users')) // Returns cached result
